6e536d5dcfed7c51df58ded3e3f9ff895133d796
JonathanRivrain <jonaternet@gmail.com>
2014-11-04 01:17:04 +0100

    Update and rename dev-image-create.sh to create.sh

--- create.sh
+++ create.sh
@@ -1,10 +1,59 @@
 #!/bin/bash
-DEF_ADDR="http://build.webos-ports.org/webos-ports/images/grouper/"
+
+def_model () {
+echo "
+  For what device do you want to create the rom ? 
+  (choose number)
+
+  1 - a500 (Acer Iconia A500)
+  2 - grouper (Asus Nexus 7 2012)
+  3 - maguro (Samsung Galaxy Nexus GSM)
+  4 - mako (LG Nexus 4)
+  0 - Cancel script
+"
+
+read device
+case $device in 
+  1) DEVICE=a500 ;;
+  2) DEVICE=grouper ;;
+  3) DEVICE=maguro ;;
+  4) DEVICE=mako ;;
+  0) exit 0 ;;
+  *) echo "pick a number between 1 and 4..."
+  exit 1 ;;
+esac
+}
+
+os_variant () {
+echo "
+What variant of WebOS do you want ?
+(choose number)
+
+  1 - webos-ports
+  2 - luneos-stable
+  0 - Cancel script
+  
+ "
+read variant
+case $variant in 
+  1) OS_VARIANT=webos-ports ;;
+  2) OS_VARIANT=luneos-stable ;;
+  0) exit 0 ;;
+  *) echo "pick a number between 1 and 2..."
+  exit 1 ;;
+esac
+}
+
+
+def_model
+os_variant
+
+DEF_ADDR="http://build.webos-ports.org/$OS_VARIANT/images/$DEVICE/"
 DATE=""
-DEF_ROOT="webos-ports-dev-image-grouper.tar.gz"
-DEF_ZIMAGE="zImage-grouper.bin"
-DEF_INITRD="initramfs-android-image-grouper.cpio.gz"
-DEF_MODULES="modules-grouper.tgz"
+DEF_ROOT="webos-ports-dev-image-$DEVICE.tar.gz"
+DEF_ZIMAGE="zImage-$DEVICE.bin"
+DEF_INITRD="initramfs-android-image-$DEVICE.cpio.gz"
+DEF_MODULES="modules-$DEVICE.tgz"
 INIT_PATCH="init.patch"
 BLKID="blkid"
 
@@ -59,17 +108,21 @@ mkdir -p root
 cd root
 
 if [ $skip_download != 1 ]; then
-    echo_b "Downloading image..."
-  curl -L $DEF_ADDR$DEF_ROOT | tar --numeric-owner -xz || fail "Failed to download the image!"
-
-    echo_b "Downloading modules..."
-    curl -L $DEF_ADDR$DEF_MODULES | tar --numeric-owner -xz || fail "Failed to download the modules!"
-
+    echo_b "Downloading then upacking root filesystem image..."
+    
+    wget -c $DEF_ADDR$DEF_ROOT 
+    tar xvf $DEF_ROOT --numeric-owner
+# was previously curl -L $DEF_ADDR$DEF_ROOT | tar --numeric-owner -xz || fail "Failed to download the image!"
+# But it failed every second try with my irregular connexion. wget -c prevents this issue.
+    echo_b "Downloading then unpacking modules..."
+    wget -c $DEF_ADDR$DEF_MODULES 
+    tar xvf $DEF_MODULES --numeric-owner
+    
     echo_b "Downloading zImage..."
-    curl -L $DEF_ADDR$DEF_ZIMAGE -o "boot/zImage-grouper.bin" || fail "Failed to download zImage!"
+    wget -c $DEF_ADDR$DEF_ZIMAGE -O "boot/zImage-grouper.bin"
 
     echo_b "Downloading initrd..."
-    curl -L $DEF_ADDR$DEF_INITRD -o "boot/initrd.cpio.gz" || fail "Failed to download initrd!"
+    wget -c $DEF_ADDR$DEF_INITRD -O "boot/initrd.cpio.gz"
 fi
 
 echo_b "Patching init..."
@@ -81,10 +134,12 @@ cd boot/initrd
 cp ../initrd.cpio.gz ./
 gzip -d initrd.cpio.gz || fail "Failed to unpack initrd (gzip)"
 cpio -i < initrd.cpio || fail "Failed to unpack initrd (cpio)"
-rm initrd.cpio
+rm initrd.cpio 
+
 
 # WebOS busybox does not have blkid. Thank god it has traceroute.
 cp -a ../../../$BLKID bin/blkid
+# I don't think this is still necessary
 patch -p1 < ../../../$INIT_PATCH || fail "Failed to patch init!"
 (find | sort | cpio --quiet -o -H newc ) | gzip > ../initrd.img || fail "Failed to pack initrd!"
 cd ../..
@@ -106,6 +161,8 @@ zip_name="../${ZIP_DEST}$(date +%Y%m%d).mrom"
 if [ -r $zip_name ]; then 
     rm $zip_name
 fi
+rm $DEF_MODULES $DEF_ROOT
 zip -0 -r $zip_name ./* || fail "Failed to create final ZIP!"
 
 echo_b "Success!"
+
